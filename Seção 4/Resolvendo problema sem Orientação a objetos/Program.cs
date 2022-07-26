1ffff4dbbbec629e3fd47fedda45a0c40b4f60c1
Victron012 <victron.dafonte@gmail.com>
2022-07-26 16:47:22 -0300

    Aula 38

---
+++ Seção 4/Resolvendo problema sem Orientação a objetos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace Resolvendo_problema_sem_Orientação_a_objetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, p, areaX, areaY;
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+            c = double.Parse(Console.ReadLine());
+            p = (a + b + c) / 2;
+
+            areaX = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+            c = double.Parse(Console.ReadLine());
+            p = (a + b + c) / 2;
+
+            areaY = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+
+            Console.WriteLine("Área de X = {0}", areaX.ToString("0.0000"), CultureInfo.InvariantCulture);
+            Console.WriteLine("Área de Y = {0}", areaY.ToString("0.0000"), CultureInfo.InvariantCulture);
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior área: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área: Y");
+            }
+        }
+    }
+}
