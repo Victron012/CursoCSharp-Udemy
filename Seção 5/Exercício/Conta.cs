74d84321e692508b07ba57ff176524af6604a86c
Victron012 <victron.dafonte@gmail.com>
2022-07-29 14:13:27 -0300

    finalizacao secao 5

---
+++ Seção 5/Exercício/Conta.cs
@@ -0,0 +1,34 @@
+﻿namespace Exercício
+{
+    class Conta
+    {
+        public int NumeroConta { get; private set; }
+        public string TitularConta { get; set; }
+        public double Saldo { get; private set; }
+
+        public Conta(int numeroConta, string titularConta)
+        {
+            NumeroConta = numeroConta;
+            TitularConta = titularConta;
+        }
+        public Conta(int numeroConta, string titularConta, double depositoInicial) : this(numeroConta, titularConta)
+        {
+            Deposito(depositoInicial);
+        }
+
+        public void Deposito(double valor)
+        {
+            if (valor > 0) { Saldo += valor; }
+        }
+
+        public void Saque(double valor)
+        {
+            if (valor > 0) { Saldo -= (valor+5); }
+        }
+
+        public override string ToString()
+        {
+            return "Conta: "+NumeroConta+", Titular: "+TitularConta+", Saldo: R$"+Saldo.ToString("0.00");
+        }
+    }
+}
---
+++ Seção 5/Exercício/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Exercício
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta;
+            int numConta;
+            string titularConta, decisao;
+            double valor;
+            Console.Write("Entre o número da conta: ");
+            numConta = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            titularConta = Console.ReadLine();
+            Console.Write("Haverá depósito inicial? (S/N): ");
+            decisao = Console.ReadLine().ToUpper();
+            while (decisao != "S" && decisao != "N")
+            {
+                Console.WriteLine("Opção inválida!\nDigite novamente:");
+                decisao = Console.ReadLine().ToUpper();
+            }
+            if (decisao == "S")
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                valor = double.Parse(Console.ReadLine());
+                conta = new Conta(numConta, titularConta, valor);
+            }
+            else
+            {
+                conta = new Conta(numConta, titularConta);
+            }
+
+            Console.WriteLine("Dados da conta:\n"+conta);
+            Console.Write("Entre um valor para depósito: ");
+            conta.Deposito(double.Parse(Console.ReadLine()));
+            Console.WriteLine("Dados da conta atualizados:\n"+conta);
+            Console.Write("Entre um valor para saque: ");
+            conta.Saque(double.Parse(Console.ReadLine()));
+            Console.WriteLine("Dados da conta atualizados:\n" + conta);
+        }
+    }
+}
